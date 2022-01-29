69e65515464ed4f956e1c34ab2ffe7bd85cdf2bc
Vladimir Kukarekin <unlock.onliner@gmail.com>
2022-01-29 19:39:14 +0300

    Comparison with Equality Operator

---
+++ freecodecamp/Comparison with the Equality Operator.js
@@ -0,0 +1,28 @@
+// Setup
+function testEqual(val) {
+    if (val == 12) { // Change this line
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+testEqual(10);
+
+// Setup
+function testStrict(val) {
+    if (val === 7) { // Change this line
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+testStrict(10);
+// Setup
+function compareEquality(a, b) {
+    if (a === b) { // Change this line
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+compareEquality(10, "10");
