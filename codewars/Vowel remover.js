749d11e9cf9d6ce6be2403ec8aef5901a8d8a8dd
Vladimir Kukarekin <unlock.onliner@gmail.com>
2022-01-29 12:05:55 +0300

    test done

--- codewars/Vowel remover.js
+++ codewars/Vowel remover.js
@@ -1,7 +1,7 @@
 function shortcut(string) {
     let a = ['a', 'o', 'e', 'u', 'i']
-    for (let i = 0; i <= string.length; i++) {
-        for (let b = 0; b <= string.length; b++) {
+    for (let i = 0; i < string.length; i++) {
+        for (let b = 0; b < 6; b++) {
             string = string.replace(a[b], "")
         }
 
