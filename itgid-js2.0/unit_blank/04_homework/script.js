335f00a333b9fd806078c57ac051a71492cc3b71
Vladimir Kukarekin <unlock.onliner@gmail.com>
2022-03-02 12:03:04 +0300

    whether

---
+++ itgid-js2.0/unit_blank/04_homework/script.js
@@ -0,0 +1,18 @@
+function out(id, name) {
+    console.log(name)
+    document.querySelector(id).innerHTML = name
+}
+
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=53.9024716&lon=27.5618225&appid=084e28bdc767259b68f992081d2a1a4b')
+
+.then(function(resp) {
+        return resp.json()
+    })
+    .then(function(data) {
+
+        console.log(data)
+        out(".city_name", data.name)
+        out(".temp", `${Math.floor(data.main.temp - 273.15)} &#176;`)
+        out('.oblaka', `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png">`)
+
+    })
